b5db3b0d8d752dab11d180ec5286d7121c2366e7
Pranav Nijampurkar <pratham.1612@gmail.com>
2021-02-02 14:17:01 +0900

    1st commit

---
+++ Analyser/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Analyser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Combo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnInfo(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HandleUnchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HandleThirdState(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
